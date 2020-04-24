e260da934d44bdae71ac93a4837193c6da703b36
JoonBeomLee <jbl9595@naver.com>
2020-04-24 20:20:20 +0900

    3_for ~ing

---
+++ src/python/3_for문/10950.py
@@ -0,0 +1,7 @@
+count = int(input())
+
+for i in range(count):
+    a, b  = input().split(" ")
+    a = int(a); b = int(b)
+    
+    print(a + b)
---
+++ src/python/3_for문/8393.py
@@ -0,0 +1,7 @@
+max_num = int(input())
+sum_all = 0
+
+for i in range(1, max_num + 1):
+    sum_all = sum_all + i
+    
+print(sum_all)
