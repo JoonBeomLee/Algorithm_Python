2e05b5391c06f969487261d0a14b943602d9994f
JoonBeomLee <jbl9595@naver.com>
2020-05-19 20:20:20 +0900

    10_수학02 ~ing

---
+++ src/python/10_수학02/2581.py
@@ -0,0 +1,20 @@
+def isPrime(num):
+    if num == 1: return False
+    
+    for i in range(2, num):
+        if num % i == 0: 
+            return False
+    
+    return True
+
+st = int(input())
+ed = int(input())
+sum_prime = 0
+list_prime = []
+
+for num in range(st, ed+1):
+    if isPrime(num): 
+        sum_prime += num
+        list_prime.append(num)
+        
+print("-1" if sum_prime == 0 else f"{sum_prime}\n{min(list_prime)}")
