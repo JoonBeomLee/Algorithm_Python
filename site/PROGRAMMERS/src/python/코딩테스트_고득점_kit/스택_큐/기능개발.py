f5f93b3fbb95c91b77be48838fb9170f6dd379e3
JoonBeomLee <jbl9595@naver.com>
2020-08-18 20:20:20 +0900

    기능개발 ~ing

--- site/PROGRAMMERS/src/python/코딩테스트_고득점_kit/스택_큐/기능개발.py
+++ site/PROGRAMMERS/src/python/코딩테스트_고득점_kit/스택_큐/기능개발.py
@@ -1,22 +1,25 @@
 def solution(progresses, speeds):
     answer = []
+    job_scheduler = [x for x in progresses]
+        
+    print(job_scheduler)
     
+    """"""
     while True:
         # 처리할 작업이 없을경우 종료
-        if not progresses: break
+        if not job_scheduler: break
             
         print(progresses)
             
         done_count = 0
         for p_idx in range(len(progresses)):
-            progresses[p_idx] += speeds[p_idx]
+            job_scheduler[p_idx] += speeds[p_idx]
             
-            if progresses[p_idx] >= 100: 
-                del(progresses[p_idx])
-                del(speeds[p_idx])
+            if job_scheduler[p_idx] >= 100: 
+                del(job_scheduler[p_idx])
                 done_count += 1
                 
         if done_count:
-            answer.append(done_count) 
-            
+            answer.append(done_count)
+    
     return answer
