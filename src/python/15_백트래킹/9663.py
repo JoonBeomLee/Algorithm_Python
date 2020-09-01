b3a0e92c34a1fe623e8fd9b889bf22a99e748c29
JoonBeomLee <jbl9595@naver.com>
2020-09-01 15:58:46 +0900

    9663 done

--- src/python/15_백트래킹/9663.py
+++ src/python/15_백트래킹/9663.py
@@ -1,55 +1,25 @@
 N = int(input())
-grid = [[True for _ in range(N)] for _ in range(N)]
+board = [0 for _ in range(N)]
+queen_count = 0
 
-def dfs_queen():
-    queen_count = 0
+def validation_board(row):
+    if row < 1: return True
+    for i in range(row):
+        if board[row] == board[i] or row - i == abs(board[row] - board[i]): return False
     
-    def put_queen(i, j, board):
-        right_up_dash = i + j
-        right_down_dash = abs(i - j)
-        
-        for row in range(N):
-            for col in range(N):
-                if row == i: 
-                    board[row][col] = False
-                elif col == j: 
-                    board[row][col] = False
-                elif row + col == right_up_dash:
-                    board[row][col] = False
-                elif abs(row-col) == right_down_dash:
-                    board[row][col] = False
+    return True
 
-        
-    def check_board(x, y):
-        board = [[True for _ in range(N)] for _ in range(N)]
-        count = 1
-        print(f'======== check_board start ========')
-        print(f'queen pos : board[{x}][{y}]')
-        put_queen(x, y, board)
-        for i in range(N):
-            for j in range(N):
-                if board[i][j]:
-                    print(f'queen pos : board[{i}][{j}]')
-                    put_queen(i, j, board)
-                    count += 1
-        print(board)
-        print(f'======== check_board end count :: {count} ========\n\n')
-        return count
+def N_Queen(row=-1):
+    global queen_count
     
-    def check_grid():
-        total_count = 0
-        tmp_count = 0
-        
-        for grid_x in range(N):
-            for grid_y in range(N):
-                grid[grid_x][grid_y] = False
-                tmp_count = check_board(grid_x, grid_y)
-                if tmp_count == N: total_count += 1
-        
-        return total_count
-    
-    queen_count = check_grid()
-    print(queen_count)
-    
-print("search start")
-dfs_queen()
+    if validation_board(row):
+        if row == N - 1: 
+            print(board)
+            queen_count += 1
+        else:
+            for i in range(N):
+                board[row + 1] = i
+                N_Queen(row+1)
+
+N_Queen()
+print(queen_count)
