87f7e79a50445bc76480095c67139d632f4b4c37
JoonBeomLee <jbl9595@naver.com>
2020-09-08 22:00:02 +0900

    위장 ~ing

--- site/PROGRAMMERS/src/python/코딩테스트_고득점_kit/해시/위장.py
+++ site/PROGRAMMERS/src/python/코딩테스트_고득점_kit/해시/위장.py
@@ -1,8 +1,5 @@
 import collections
 
-def filter_clothes(data):
-    return data[1]
-
 def solution(clothes):
     answer = 0
     catg_list = []
@@ -16,6 +13,19 @@ def solution(clothes):
     for ctg in collections.Counter(catg_list):
         tmp_list.append([ x for x in range(int(catg_dict[ctg])) ])
     
-    print(tmp_list)
-    
+    if len(tmp_list) == 1: 
+        answer = len(tmp_list[0])
+    elif len(tmp_list) == 2:
+        answer_tmp = 1
+        for tmp in tmp_list:
+            answer += len(tmp)    
+            answer_tmp *= (len(tmp))
+            
+        answer = answer + answer_tmp
+    else:
+        answer_tmp = 1
+        for tmp in tmp_list:
+            answer += len(tmp)    
+            answer_tmp *= (len(tmp) + 1)
+        
     return answer
