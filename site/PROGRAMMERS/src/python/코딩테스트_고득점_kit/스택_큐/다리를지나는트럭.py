146ca1d492e130309c7a1d9b36a41a595fe46a3e
JoonBeomLee <jbl9595@naver.com>
2020-09-21 23:46:05 +0900

    다리를지나가는트럭 ~ing

---
+++ site/PROGRAMMERS/src/python/코딩테스트_고득점_kit/스택_큐/다리를지나는트럭.py
@@ -0,0 +1,10 @@
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    bridge = [weight]
+    
+    print(bridge)
+    print(bridge_length)
+    print(weight)
+    print(truck_weights)
+    
+    return answer
