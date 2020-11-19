31ad636753420e88e0aae16be3eb342bb28386d5
JoonBeomLee <jbl9595@naver.com>
2020-11-19 20:11:26 +0900

    주식가격 done

--- site/PROGRAMMERS/src/python/스킬테스트/level02/주식가격.py
+++ site/PROGRAMMERS/src/python/스킬테스트/level02/주식가격.py
@@ -13,20 +13,3 @@ def solution(prices):
         answer.append(tmp_sum)
         
     return answer
-
-"""
-def solution(prices):
-    answer = []
-    
-    for price_idx in range(len(prices)):
-        tmp_sum = 0
-        
-        for answer_idx in range(price_idx + 1, len(prices)):
-            tmp_sum += 1 
-            
-            if prices[price_idx] > prices[answer_idx]: break 
-                
-        answer.append(tmp_sum)
-
-    return answer
-"""
