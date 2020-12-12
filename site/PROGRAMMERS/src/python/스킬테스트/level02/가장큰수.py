ef0db04e8c5547426916386478a38b22272ef051
JoonBeomLee <jbl9595@naver.com>
2020-12-12 21:53:40 +0900

    가장큰수 - 시간초과 해결중

--- site/PROGRAMMERS/src/python/스킬테스트/level02/가장큰수.py
+++ site/PROGRAMMERS/src/python/스킬테스트/level02/가장큰수.py
@@ -1,2 +1,9 @@
-def solution(args):
-    return False  
+from itertools import permutations
+
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers = list(permutations(numbers, len(numbers)))
+    numbers = list(map(lambda num: ''.join(num), numbers)) 
+    numbers = list(map(int, numbers))
+    
+    return str(max(numbers))
