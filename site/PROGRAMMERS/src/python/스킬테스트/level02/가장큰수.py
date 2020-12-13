ce0f8034e3e68dd5bad383a18508cda7fa6c7a65
JoonBeomLee <jbl9595@naver.com>
2020-12-13 15:34:58 +0900

    가장큰수 done

--- site/PROGRAMMERS/src/python/스킬테스트/level02/가장큰수.py
+++ site/PROGRAMMERS/src/python/스킬테스트/level02/가장큰수.py
@@ -1,9 +1,8 @@
-from itertools import permutations
-
 def solution(numbers):
     numbers = list(map(str, numbers))
-    numbers = list(permutations(numbers, len(numbers)))
-    numbers = list(map(lambda num: ''.join(num), numbers)) 
-    numbers = list(map(int, numbers))
+    # *3 의 이유
+    # numbers의 인수 값인 num이 1000이하의 수이므로 
+    # 같은 비교를 위해 3자리 수로 맞춘뒤 비교 위함
+    numbers.sort(key = lambda num : num * 3, reverse = True)
     
-    return str(max(numbers))
+    return str(int(''.join(numbers)))
