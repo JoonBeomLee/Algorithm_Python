3c6485c7f1b3c916e331fc3b36eab23b8c647837
JoonBeomLee <jbl9595@naver.com>
2021-02-16 00:24:28 +0900

    가장큰정사각형찾기 done

--- site/PROGRAMMERS/src/python/스킬테스트/level02/가장큰정사각형찾기.py
+++ site/PROGRAMMERS/src/python/스킬테스트/level02/가장큰정사각형찾기.py
@@ -1,17 +1,19 @@
 def solution(board):
-    answer = 1234
-    
-    new_board = []
+    answer = 0
     
     for h_idx in range(len(board)):
-        tmp_floor = []
         
         for w_idx in range(len(board[0])):
             
             # [i, j] = min([i-1, j-1], [i-1, j], [i,j-1]) + 1
             if board[h_idx][w_idx] != 0:
-                board[h_idx][w_idx] = min(board[h_idx - 1][w_idx - 1], board[h_idx - 1][w_idx], board[h_idx][w_idx - 1]) + 1
+                
+                left = board[h_idx][w_idx - 1] if w_idx - 1 >= 0 else 0  
+                up = board[h_idx - 1][w_idx]  if h_idx - 1 >= 0 else 0
+                dash = board[h_idx - 1][w_idx - 1] if h_idx - 1 >= 0 and w_idx - 1 >= 0 else 0
+                
+                board[h_idx][w_idx] = min(dash, left, up) + 1
 
-    answer = max(max(board))   
+    answer = max(map(max, board))
         
     return answer * answer
