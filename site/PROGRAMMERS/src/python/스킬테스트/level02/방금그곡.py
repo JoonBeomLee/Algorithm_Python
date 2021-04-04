c2b34c98e7b7f8942d0b0965661cabd575320fdb
JoonBeomLee <jbl9595@naver.com>
2021-04-05 01:13:22 +0900

    방금그곡 테스트케이스 12 miss

--- site/PROGRAMMERS/src/python/스킬테스트/level02/방금그곡.py
+++ site/PROGRAMMERS/src/python/스킬테스트/level02/방금그곡.py
@@ -1,10 +1,12 @@
-from datetime import datetime, timedelta
-
-def solution(m, musicinfos):
-    answer = ''
-    YEAR = 2021
-    MONTH = 4
-    DAY = 4
+def solution(user_m, musicinfos):
+    answer = []
+    user_m_list = []
+    for i, m in enumerate(user_m):
+        if m == '#':
+            user_m_list[-1] += '#'
+        else:
+            user_m_list.append(m)
+    
     
     # music info 회문
     for music_info in musicinfos:
@@ -15,15 +17,41 @@ def solution(m, musicinfos):
         music_name = music_info_list[2]
         music_melody = music_info_list[3]
         
-        # melody 재조정                
+        # melody 재조정
+        # '#' 조정
+        melody_list = []
+        for i, m in enumerate(music_melody):
+            if m == '#':
+                melody_list[-1] += '#'
+            else:
+                melody_list.append(m)
+
+        # melody 재조정 - time 조정
+        re_time_h = (music_ed[0] - music_st[0] + -1) if music_ed[1] - music_st[1] < 0 else (music_ed[0] - music_st[0])
+        re_time_m = (60 + (music_ed[1] - music_st[1]) ) if music_ed[1] - music_st[1] < 0 else music_ed[1] - music_st[1]
+        re_total_time = (60 * re_time_h) + re_time_m
         
-        if len(m) > len(music_melody):
-            if music_melody in m: 
-                answer = music_name
-                break
-        else:
-            if m in music_melody: 
-                answer = music_name
+        # melody 재조정 - melody 조정
+        melody_idx = 0
+        re_melody = []
+        for idx in range(re_total_time):
+            re_melody.append(melody_list[idx % len(melody_list)])
+            
+        # melody 판단       
+        m_idx = 0        
+        for mld in re_melody:            
+            if user_m_list[m_idx] == mld: m_idx += 1
+            else: m_idx = 0
+                
+            if m_idx == len(user_m_list):                
+                answer.append([music_name, re_total_time])
                 break
                 
+    
+    re_answer = sorted(answer, key=lambda x : x[1], reverse=True)
+    
+    if len(re_answer) == 0: answer = "(None)"
+    else:
+        answer = re_answer[0][0]
+
     return answer
