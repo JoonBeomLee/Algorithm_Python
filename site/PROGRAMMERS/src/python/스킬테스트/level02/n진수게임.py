e06c6b60b318b9cb7b9c195dbf044358fe9231db
JoonBeomLee <jbl9595@naver.com>
2021-04-10 17:33:03 +0900

    n진수 게임 done

--- site/PROGRAMMERS/src/python/스킬테스트/level02/n진수게임.py
+++ site/PROGRAMMERS/src/python/스킬테스트/level02/n진수게임.py
@@ -12,12 +12,32 @@ def num_convert(num, n):
 def solution(n, t, m, p):
     answer = ''
     num_list = []
+    my_list = []
     
-    tmp = num_convert(8, 2)
+    num = 0
+    man_idx = 0
+    converted_num = "0"
     
     while True:
-        if len(num_list) >= t: break
+        if len(my_list) >= t: break
+        
+        num_list.extend(converted_num)
+        
+        for c_n in converted_num:           
+            # 본인 차례
+            if man_idx == p - 1:
+                my_list.extend(c_n)
+            
+            man_idx += 1
+            
+            # 순서 초기화
+            if man_idx == m:
+                man_idx = 0
+            
+        num += 1
+        converted_num = num_convert(num, n)
     
-    print(num_list)
+    my_list = my_list[:t]
+    answer = ''.join(my_list)
     
     return answer
