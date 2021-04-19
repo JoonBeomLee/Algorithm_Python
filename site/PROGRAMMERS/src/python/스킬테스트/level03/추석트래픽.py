3b5fad8bde82877e4a7910c407d5fed0d9299baf
JoonBeomLee <jbl9595@naver.com>
2021-04-19 22:07:33 +0900

    추석트래픽 ~ing

--- site/PROGRAMMERS/src/python/스킬테스트/level03/추석트래픽.py
+++ site/PROGRAMMERS/src/python/스킬테스트/level03/추석트래픽.py
@@ -1,26 +1,86 @@
-from datetime import datetime, timedelta
+from datetime import datetime
+from datetime import timedelta
+
+def tsToDs(ts):
+    #timestamp to datetime
+    return datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+
+def dsToTs(ds):
+    #datetime to timestamp
+    obj_datetime = datetime.strptime(ds, '%Y-%m-%d %H:%M:%S.%f')
+    return obj_datetime
+
+def addMSeconds(date, ms):
+    return date + timedelta(milliseconds=ms)
+
+def addSeconds(date, seconds):
+    return date + timedelta(seconds=seconds)
+
+def second2miliseconds(seconds):
+    seconds = float(seconds.split("s")[0])
+    
+    return seconds * 1000
 
 def solution(lines):
     answer = 0
-    traffic = {}
-    date_day = "2016-09-15"
+    t_dict = {}
     
-    for job_idx, line in enumerate(lines):
-        sp_line = line.split()
+    # data reporcess
+    for line in lines:
+        line = line.split()
         
-        T = float(sp_line[2].split("s")[0])
-        ST_time = sp_line[1].split(".")[0]
-        ST_s_datetime = "0." + sp_line[1].split(".")[1] + "000"
-        ST_s = float("0." + sp_line[1].split(".")[1])
-        ED_s = int(ST_s + T)
+        DATE = line[0]
+        ST = line[1]
+        T = line[2]
         
-        traffic_time = ""
+        st_stamp = dsToTs(f"{DATE} {ST}")
+        wk_stamp = addMSeconds(st_stamp, second2miliseconds(T))
         
-        tmp_date_s = f"{date_day} {ST_time}"
-        tmp_dt = datetime.strptime(f"{ST_time}{ST_s_datetime}", '%Y-%m-%d %H:%M:%S.%f')
+        # 소요시간
+        T_time = wk_stamp - st_stamp
+        T_time_sp = str(T_time).split(":")
+        T_time_sp = ":".join(T_time_sp[:2])
+        # ms포함일 경우
+        if "." in str(T_time): 
+            T_time_ms = str(T_time).split(":")[2].split(".")[1][:-3]
+        else:
+            T_time_ms = 0
+        T_time_s = str(T_time).split(":")[2].split(".")[0]
         
-        print(tmp_dt)
+        #print(T_time, int(T_time_s), T_time_ms)
         
+        total_time = 0
 
+        while True:
+            # total time이 주어진시간이 초과 될때
+            if total_time == int(T_time_s):
+                if float(T_time_ms) >= 0:
+                    add_stamp = addSeconds(st_stamp, total_time + 1)
+                    dict_stamp = str(add_stamp).split()[1]
+                    
+                    if "." in dict_stamp:
+                        dict_stamp = dict_stamp.split(".")[0]
+                        
+                    if dict_stamp not in t_dict:
+                        t_dict[dict_stamp] = 1
+                    else:
+                        t_dict[dict_stamp] += 1
+                break
+                
+            #print(st_stamp)
+            add_stamp = addSeconds(st_stamp, total_time)
+            dict_stamp = str(add_stamp).split()[1]
+            
+            if "." in dict_stamp:
+                dict_stamp = dict_stamp.split(".")[0]
+            
+            if dict_stamp not in t_dict:
+                t_dict[dict_stamp] = 1
+            else:
+                t_dict[dict_stamp] += 1
+    
+            total_time += 1
+    
+    print(t_dict)
     
     return answer
