dc0101274dc75e01de0e3fdd9159cac3f91abe57
JoonBeomLee <jbl9595@naver.com>
2021-04-20 21:56:20 +0900

    추석트래픽 done

--- site/PROGRAMMERS/src/python/스킬테스트/level03/추석트래픽.py
+++ site/PROGRAMMERS/src/python/스킬테스트/level03/추석트래픽.py
@@ -1,86 +1,42 @@
-from datetime import datetime
-from datetime import timedelta
-
-def tsToDs(ts):
-    #timestamp to datetime
-    return datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-
-def dsToTs(ds):
-    #datetime to timestamp
-    obj_datetime = datetime.strptime(ds, '%Y-%m-%d %H:%M:%S.%f')
-    return obj_datetime
-
-def addMSeconds(date, ms):
-    return date + timedelta(milliseconds=ms)
-
-def addSeconds(date, seconds):
-    return date + timedelta(seconds=seconds)
-
-def second2miliseconds(seconds):
-    seconds = float(seconds.split("s")[0])
+def strToMs(time):
+    hhmmss = time.split(".")[0]
+    ms = int(time.split(".")[1])
     
-    return seconds * 1000
-
+    h = int(hhmmss.split(":")[0])
+    m = int(hhmmss.split(":")[1])
+    s = int(hhmmss.split(":")[2])
+    
+    total_ms = (h * 3600 + m * 60 + s) * 1000 + ms
+    
+    return total_ms
+    
 def solution(lines):
-    answer = 0
-    t_dict = {}
+    answer = 1
+    boundary = []
     
-    # data reporcess
+    # data preprocess
     for line in lines:
-        line = line.split()
+        data = line.split()
         
-        DATE = line[0]
-        ST = line[1]
-        T = line[2]
+        date = data[0]
+        time = data[1]
+        thresh = float(data[2].split("s")[0]) * 1000
         
-        st_stamp = dsToTs(f"{DATE} {ST}")
-        wk_stamp = addMSeconds(st_stamp, second2miliseconds(T))
+        ed_ms = strToMs(time)
+        st_ms = int(ed_ms - thresh + 1)
         
-        # 소요시간
-        T_time = wk_stamp - st_stamp
-        T_time_sp = str(T_time).split(":")
-        T_time_sp = ":".join(T_time_sp[:2])
-        # ms포함일 경우
-        if "." in str(T_time): 
-            T_time_ms = str(T_time).split(":")[2].split(".")[1][:-3]
-        else:
-            T_time_ms = 0
-        T_time_s = str(T_time).split(":")[2].split(".")[0]
+        boundary.append([st_ms, ed_ms])
+    
+    # 판단
+    for b1_idx, b1 in enumerate(boundary):
+        tmp_ans = 1
         
-        #print(T_time, int(T_time_s), T_time_ms)
-        
-        total_time = 0
-
-        while True:
-            # total time이 주어진시간이 초과 될때
-            if total_time == int(T_time_s):
-                if float(T_time_ms) >= 0:
-                    add_stamp = addSeconds(st_stamp, total_time + 1)
-                    dict_stamp = str(add_stamp).split()[1]
+        for b2_idx, b2 in enumerate(boundary):
+            if b2_idx < b1_idx + 1: continue
                     
-                    if "." in dict_stamp:
-                        dict_stamp = dict_stamp.split(".")[0]
-                        
-                    if dict_stamp not in t_dict:
-                        t_dict[dict_stamp] = 1
-                    else:
-                        t_dict[dict_stamp] += 1
-                break
+            if b1[1] + 1000 > b2[0]: tmp_ans += 1
                 
-            #print(st_stamp)
-            add_stamp = addSeconds(st_stamp, total_time)
-            dict_stamp = str(add_stamp).split()[1]
-            
-            if "." in dict_stamp:
-                dict_stamp = dict_stamp.split(".")[0]
-            
-            if dict_stamp not in t_dict:
-                t_dict[dict_stamp] = 1
-            else:
-                t_dict[dict_stamp] += 1
-    
-            total_time += 1
-    
-    print(t_dict)
+        answer = tmp_ans if tmp_ans > answer else answer
+        
     
     return answer
