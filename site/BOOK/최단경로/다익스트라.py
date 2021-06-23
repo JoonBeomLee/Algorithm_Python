934f980d37eb33e83d8c0855058f8bb24daa51e8
JoonBeomLee <jbl9595@naver.com>
2021-06-23 23:49:44 +0900

    다익스트라 ~ing

--- site/BOOK/최단경로/다익스트라.py
+++ site/BOOK/최단경로/다익스트라.py
@@ -23,9 +23,44 @@ for _ in range(M):
 # 방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호 반환
 def get_smallest_node():
     min_val = INF
-    idx = 0 # 가장 최단 거리가 짧은 노드(인덱스)
+    min_idx = 0 # 가장 최단 거리가 짧은 노드(인덱스)
 
     for i in range(1, N + 1):
         if distance[i] < min_val and not visited[i]:
             min_val = distance[i]
-            index = i 
+            min_idx = i 
+
+    # 가장 짧은 노드 위치 반환
+    return min_idx
+
+def dijkstra(start):
+    # 시작 노드에 대해서 초기화
+    distance[start] = 0
+    visited[start] = True
+
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+
+    # 시작 노드를 제외한 전체 n - 1개의 노드에 대해 반복
+    for i in range(N - 1):
+        # 현재 최단 거리가 가장 짧은 노드를 꺼내서, 방문 처리
+        now = get_smallest_node()
+        visited[now] = True
+
+        # 현재 노드와 연결된 다른 노드를 확인
+        for j in graph[now]:
+            cost = distance[now] + j[1]
+
+            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+# 다익스트라 알고리즘 호출
+dijkstra(st_node)
+
+# 모든 노드로 가기 위한 최단 거리를 출력
+for i in range(1, N + 1):
+
+    # 도달할 수 없는 경우 무한 출력
+    if distance[i] == INF: print("INFINITY")
+    else: print(distance[i])
